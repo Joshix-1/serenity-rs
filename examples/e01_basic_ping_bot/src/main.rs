c44ffd80c4f2b56633d3cb7587bed57c0e84d2ec
Joshix <joshix.dev@mailbox.org>
2026-01-26 18:00:00 +0000

    rework example

--- examples/e01_basic_ping_bot/src/main.rs
+++ examples/e01_basic_ping_bot/src/main.rs
@@ -1,46 +1,35 @@
 use std::sync::Arc;
 
-use serenity::async_trait;
-use serenity::gateway::client::FullEvent;
+use serenity::all::GenericEventHandler;
 use serenity::prelude::*;
 
 struct Handler;
 
-#[async_trait]
-impl EventHandler for Handler {
-    async fn dispatch(&self, ctx: &Context, event: &FullEvent) {
-        match event {
-            // Set a handler for the `message` event. This is called whenever a new message is
-            // received.
-            //
-            // Event handlers are dispatched through a threadpool, and so multiple events can be
-            // dispatched simultaneously.
-            FullEvent::Message {
-                new_message, ..
-            } => {
-                if new_message.content == "!ping" {
-                    // Sending a message can fail, due to a network error, an authentication error,
-                    // or lack of permissions to post in the channel, so log to
-                    // stdout when some error happens, with a description of it.
-                    if let Err(why) = new_message.channel_id.say(&ctx.http, "Pong!").await {
-                        println!("Error sending message: {why:?}");
-                    }
-                }
-            },
-
-            // Set a handler to be called on the `ready` event. This is called when a shard is
-            // booted, and a READY payload is sent by Discord. This payload contains
-            // data like the current user's guild Ids, current user data, private
-            // channels, and more.
-            //
-            // In this case, just print what the current user's username is.
-            FullEvent::Ready {
-                data_about_bot, ..
-            } => {
-                println!("{} is connected!", data_about_bot.user.name);
-            },
-            _ => {},
+impl GenericEventHandler for Handler {
+    // Set a handler for the `message` event. This is called whenever a new message is
+    // received.
+    //
+    // Event handlers are dispatched through a threadpool, and so multiple events can be
+    // dispatched simultaneously.
+    async fn on_message(&self, ctx: &Context, event: serenity::all::event::MessageEventRef<'_>) {
+        if event.new_message.content == "!ping" {
+            // Sending a message can fail, due to a network error, an authentication error,
+            // or lack of permissions to post in the channel, so log to
+            // stdout when some error happens, with a description of it.
+            if let Err(why) = event.new_message.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {why:?}");
+            }
         }
+    }
+
+    // Set a handler to be called on the `ready` event. This is called when a shard is
+    // booted, and a READY payload is sent by Discord. This payload contains
+    // data like the current user's guild Ids, current user data, private
+    // channels, and more.
+    //
+    // In this case, just print what the current user's username is.
+    async fn on_ready(&self, _ctx: &Context, event: serenity::all::event::ReadyEventRef<'_>) -> () {
+        println!("{} is connected!", event.data_about_bot.user.name);
     }
 }
 
